7d09b49bda00a709b54f1972d957cd2a37ff580d
Autumn2016 <autumn@romenscd.cn>
2016-06-06 16:51:36 +0800

    代码小例子。

--- examples/arr-pop.js
+++ examples/arr-pop.js
@@ -2,3 +2,10 @@ var tmp = [1,2,3,4];
 
 var arr = tmp.pop();
 console.log(arr);
+
+var tmp = {"Content":"QUERY_AUTH_CODE:queryauthcode@@@f9KglKDbf_LAkTU-8YMwCKexhTcUrsleqaE8mCSJcmlZ-x1irS9pUPRYhWwd-H9Yn9yaoI2JAt-IlgCozOCBxg"};
+var content = tmp.Content;
+console.log(content.substring(0, 15));
+
+var code = content.split(':').pop();
+console.log(code);
---
+++ underscore.js
@@ -0,0 +1,8 @@
+var underscore = require('underscore');
+var str = '';
+var arr = [];
+var obj = {};
+
+console.log(underscore.isEmpty(str));
+console.log(underscore.isEmpty(arr));
+console.log(underscore.isEmpty(obj));
